c79873671a3870337a44f8ea0e5a94536f96d932
Phil Pinsdorf <philemon.pinsdorf@live.de>
2024-06-08 09:26:00 +0200

    Added Docker containerisation

---
+++ Dockerfile
@@ -0,0 +1,7 @@
+FROM node:alpine
+WORKDIR /usr/src/app
+COPY . .
+RUN npm install
+RUN tsc
+EXPOSE 3000
+CMD ["node", "./out/server.js"]
--- src/server.ts
+++ src/server.ts
@@ -6,9 +6,9 @@ export const rooms: Map<string, Room> = new Map<string, Room>();
 export const used_ids: number[] = [];
 export const ROOM_CODE_LENGTH: number = 4;
 
-const port: number = 3000;
-const wss = new WebSocket.Server({ port: port });
-console.log(`WebSocket Server started on Port ${port}`);
+const PORT: number = 3000;
+const wss = new WebSocket.Server({ port: PORT });
+console.log(`WebSocket Server started on Port ${PORT}`);
 
 wss.on('connection', (ws: WebSocket) => {
     console.log(`New client connected.`);
